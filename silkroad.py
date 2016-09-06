44e4aec195765a6a1fbbd3b665d0cfeb7a93c729
SirCaptainMitch <ubuntu@mail.sgc.com>
2016-09-06 15:07:11 +0000

    removing : from the call

--- silkroad.py
+++ silkroad.py
@@ -18,7 +18,7 @@ slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
 BOT_ID = os.environ.get("BOT_ID")
 
 # constants
-AT_BOT = "<@" + BOT_ID + ">:"
+AT_BOT = "<@" + BOT_ID + ">"
 EXAMPLE_COMMAND = "do"
 
 
